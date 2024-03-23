4f110d988de1862ef6dc234c80e0f2e348859cd5
pplociennik <pplociennik097@gmail.com>
2024-03-23 16:59:52 +0100

    [NO_JIRA] Moved ResourceNotFound exception handling to commons.

--- src/main/java/com/goaleaf/accounts/controller/UserControllerAdvice.java
+++ src/main/java/com/goaleaf/accounts/controller/UserControllerAdvice.java
@@ -1,15 +1,7 @@
 package com.goaleaf.accounts.controller;
 
-import com.github.pplociennik.commons.dto.ErrorResponseDto;
 import com.github.pplociennik.commons.exc.GlobalExceptionHandler;
-import com.github.pplociennik.commons.exc.ResourceNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-
-import java.time.ZonedDateTime;
 
 /**
  * A controller advice being the global exception handler for the {@link UserController}.
@@ -18,26 +10,5 @@ import java.time.ZonedDateTime;
  */
 @ControllerAdvice( assignableTypes = { UserController.class } )
 class UserControllerAdvice extends GlobalExceptionHandler {
-
-    /**
-     * Handles the {@link ResourceNotFoundException}.
-     *
-     * @param aException
-     *         the exception being thrown during the system work.
-     * @param aWebRequest
-     *         the web request which execution was interrupted by the exception.
-     * @return {@link ErrorResponseDto}.
-     */
-    @ExceptionHandler( ResourceNotFoundException.class )
-    public ResponseEntity< ErrorResponseDto > handleResourceNotFoundException( ResourceNotFoundException aException,
-                                                                               WebRequest aWebRequest ) {
-        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
-                aWebRequest.getDescription( false ),
-                HttpStatus.NOT_FOUND,
-                aException.getMessage(),
-                ZonedDateTime.now()
-        );
-        return new ResponseEntity<>( errorResponseDTO, HttpStatus.NOT_FOUND );
-    }
 
 }
