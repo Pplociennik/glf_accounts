51ebf6520d5b6ae9b95ed58a05b7cef2e7dd135c
pplociennik <pplociennik097@gmail.com>
2024-03-23 18:09:15 +0100

    [NO_JIRA] Modification in schemas' documentation.

--- src/main/java/com/goaleaf/accounts/dto/UserDto.java
+++ src/main/java/com/goaleaf/accounts/dto/UserDto.java
@@ -12,7 +12,7 @@ import lombok.EqualsAndHashCode;
  * @author Created by: Pplociennik at 14.03.2024 19:00
  */
 @Schema(
-        name = "User",
+        name = "User Data transfer Object",
         description = "A schema representing user data."
 )
 @EqualsAndHashCode( callSuper = true )
@@ -20,15 +20,23 @@ import lombok.EqualsAndHashCode;
 @AllArgsConstructor
 public class UserDto extends BaseAbstractExtendableDto {
 
-    @Schema( description = "The unique identifier of the user.", example = "e984751d-41a8-410a-91f0-3180ef459d1d" )
+    @Schema(
+            description = "The unique identifier of the user.",
+            example = "e984751d-41a8-410a-91f0-3180ef459d1d" )
     private String userId;
 
-    @Schema( description = "The username.", example = "Test Username" )
+    @Schema(
+            description = "The username.",
+            example = "Test Username" )
     private String userName;
 
-    @Schema( description = "The email address.", example = "test@testmail.com" )
+    @Schema(
+            description = "The email address.",
+            example = "test@testmail.com" )
     private String emailAddress;
 
-    @Schema( description = "The description of the user.", example = "Hi, this is the test user description." )
+    @Schema(
+            description = "The description of the user.",
+            example = "Hi, this is the test user description." )
     private String description;
 }
